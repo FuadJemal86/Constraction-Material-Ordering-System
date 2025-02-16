fb03bb9fef23b72f4402b87662c1ea33f2c5a962
trip <fuad47722@gmail.com>
2025-02-16 15:52:27 +0300

    refactor: restructure home components and update index.html title

--- cliaynt/src/App.jsx
+++ cliaynt/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Body from "../components/homeComponents/Body";
-import './index.css';
+import Body from './components/homeComponents/Body';
+
+
 
 function App() {
     return (
--- cliaynt/src/components/homeComponents/Body.jsx
+++ cliaynt/src/components/homeComponents/Body.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import '../../src/index.css'
+
 
 
 
 function Body() {
     return (
         <div>
-            <div className='text-6xl font-bold underline'>
+            <div className='text-red-600 font-bold underline'>
                 hellow
             </div>
         </div>
