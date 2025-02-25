e1ed99e61039f261453edcf72534344071aa10d1
trip <fuad47722@gmail.com>
2025-02-25 21:14:21 +0300

    refactor: remove unnecessary text from Body component

--- cliaynt/src/components/homeComponents/Body.jsx
+++ cliaynt/src/components/homeComponents/Body.jsx
@@ -6,9 +6,7 @@ import React from 'react';
 function Body() {
     return (
         <div>
-            <div className='text-red-600 font-bold underline'>
-                hellow
-            </div>
+
         </div>
     )
 }
