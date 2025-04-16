dae73007e5b69edfa5840bb7966ef55d10980c66
trip <fuad47722@gmail.com>
2025-04-16 22:59:29 +0300

    done customer update password

--- server/controllers/customer controllers/update.password.js
+++ server/controllers/customer controllers/update.password.js
@@ -1,4 +1,6 @@
-
+const jsw = require('jsonwebtoken')
+const prisma = require('../../prismaCliaynt')
+const bcrypt = require('bcrypt')
 
 const updatePassword = async (req, res) => {
 
@@ -7,10 +9,25 @@ const updatePassword = async (req, res) => {
 
     const token = req.cookies['t-auth-token']
 
-    if(!token) {
-        return res.status(400).json({status: false , message: 'token not provide'})
+    if (!token) {
+        return res.status(400).json({ status: false, message: 'token not provide' })
     }
 
+    const decoded = jsw.verify(token, process.env.CUSTOMER_KEY)
 
+    const id = parseInt(decoded.id)
 
+    const hashPssword = bcrypt.hash(password, 10)
+
+    try {
+        await prisma.customer.update({
+            where: { id: id },
+            data: { hashPssword }
+        })
+    }catch(err) {
+        console.log(err)
+        return res.status(500).json({status: false , message: 'server error'})
+    }
 }
+
+module.exports = {updatePassword}
