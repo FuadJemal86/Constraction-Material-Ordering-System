725863055b9eb4fa2d189cab3313fa88b0f19d1e
Fuad Jemal <fuad47722@gmail.com>
2025-07-04 08:13:48 +0300

    fix: Add comment to

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: ['http://localhost:5174', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'UPDATE'],
-    credentials: true
+    credentials: true //true
 }));
 
 app.use(express.static('public'));
