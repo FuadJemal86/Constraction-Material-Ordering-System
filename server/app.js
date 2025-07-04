3febda0aeb90c14e50b4dbc3a052ebef35731a0d
Fuad Jemal <fuad47722@gmail.com>
2025-07-04 08:16:58 +0300

    cancel

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: ['http://localhost:5174', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'UPDATE'],
-    credentials: true //true
+    credentials: true
 }));
 
 app.use(express.static('public'));
