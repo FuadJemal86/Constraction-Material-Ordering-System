0dd105622d73893bdbd5ace3524b1f51333e024c
Fuad Jemal <fuad47722@gmail.com>
2025-07-05 22:06:13 +0300

    add path to logout

--- server/controllers/customer controllers/logOut.js
+++ server/controllers/customer controllers/logOut.js
@@ -1,13 +1,12 @@
 const logout = (req, res) => {
     const isProduction = process.env.NODE_ENV === "production";
 
-    res.clearCookie('x-auth-token', {
+    res.clearCookie("x-auth-token", {
         httpOnly: true,
         secure: isProduction,
         sameSite: isProduction ? "none" : "lax",
+        path: "/", // this must match how it was set
     });
 
-    res.status(200).json({ message: "Logout successful" });
+    res.status(200).json({ message: "Logged out successfully" });
 };
-
-module.exports = { logout };
