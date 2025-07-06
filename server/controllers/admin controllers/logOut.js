929a8d2098269b7516421767f5e1edcc3c2f118a
Fuad Jemal <fuad47722@gmail.com>
2025-07-06 09:43:46 +0300

    refactor logout function to remove unnecessary variable and standardize response format

--- server/controllers/admin controllers/logOut.js
+++ server/controllers/admin controllers/logOut.js
@@ -1,4 +1,3 @@
-const isProduction = process.env.NODE_ENV === "production";
 
 
 const logout = (req, res) => {
@@ -7,7 +6,8 @@ const logout = (req, res) => {
         secure: process.env.NODE_ENV === "production",
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     });
-    res.sendStatus(200);
+    res.status(200).json({ message: "Logged out successfully" });
+
 }
 
 
